c67de6b86e5f98ae49e71caf2b9be6a2e1cdddf5
Pritam Saha <saha.pritam.work@gmail.com>
2023-10-02 19:45:15 +0530

    Added a method in General.java to handle the (post) submit request whenever a new user fills up the registration form and submits. Note :- The method is not yet fully complete it was just a testing. Also added the URL as action in the form tag in registrationPage.html

--- contactManager/src/main/java/org/pritam/contactManager/controller/General.java
+++ contactManager/src/main/java/org/pritam/contactManager/controller/General.java
@@ -1,7 +1,10 @@
 package org.pritam.contactManager.controller;
 
+import org.pritam.contactManager.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class General {
@@ -13,5 +16,11 @@ public class General {
 	@GetMapping("/getRegistered")
 	public String getRegistered() {
 		return "registrationPage";
+	}
+	
+	@PostMapping("/getRegistered")
+	public String getRegistered(@ModelAttribute User user) {
+		System.out.println(user);
+		return "home";
 	}
 }
