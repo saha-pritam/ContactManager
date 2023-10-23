92ebebf0329b7134a24915c80ed6eb2aabfdf000
Pritam Saha <saha.pritam.work@gmail.com>
2023-10-23 12:54:07 +0530

    Added authenticatedBase.html which will act as a thymeleaf parent template for every authenticated web pages. To test it out I have also added showContacts.html

--- contactManager/src/main/java/org/pritam/contactManager/controller/AuthenticatedGeneral.java
+++ contactManager/src/main/java/org/pritam/contactManager/controller/AuthenticatedGeneral.java
@@ -1,17 +1,32 @@
 package org.pritam.contactManager.controller;
 
+import org.pritam.contactManager.entity.User;
+import org.pritam.contactManager.security.UserDetailsImpl;
+import org.pritam.contactManager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/authenticated")
 public class AuthenticatedGeneral {
 	
+	@Autowired
+	private UserService userService;
+	
+	@ModelAttribute
+	public void commonAttributes(Model model) {
+		UserDetailsImpl userDetailsImpl = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		User user = userService.getByEmail(userDetailsImpl.getUsername());
+		model.addAttribute("userFirstName", user.getFirstName());
+	}
+	
 	@GetMapping("/showContacts")
-	@ResponseBody
-	public String tessignin() {
-		return "This is show contacts page";
+	public String showContacts() {
+		return "showContacts";
 	}
 }
